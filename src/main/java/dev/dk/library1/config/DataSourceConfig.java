8e3d8c96962d9bcb0911fd386a4f1e89d493deed
fbt19 <fbt0159@naver.com>
2023-05-03 21:19:47 +0900

    Initial commit

---
+++ src/main/java/dev/dk/library1/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package dev.dk.library1.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource(){return DataSourceBuilder.create().type(HikariDataSource.class).build();}
+
+
+
+}
---
+++ src/main/java/dev/dk/library1/config/MybatisConfig.java
@@ -0,0 +1,43 @@
+package dev.dk.library1.config;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MybatisConfig {
+
+    @Autowired
+    @Qualifier("dataSource")
+    DataSource dataSource;
+
+
+    @Bean
+    @Primary
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+
+        sessionFactory.setDataSource(dataSource);
+        //맵퍼 xml 파일 경로 설정
+        Resource[] res = new PathMatchingResourcePatternResolver()
+                .getResources("classpath*:/dev/dk/library1/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSession sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactoryBean());
+    }
+}
